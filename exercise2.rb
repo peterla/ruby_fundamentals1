74e275d09226340ce557b7bbcc749b8b8061dc8d
Giang La <peter.la@peterla.com>
2016-03-22 16:17:58 -0400

    Exercise 2.

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+# Question 1
+# 18% tip
+puts 55 * 1.18
+
+# Question 2
+puts "The answer is " + 88.to_s + " mph."
+
+# Question 3
+puts "The value of 45628 x 7839 is #{45628 * 7839}."
+
+# Question 4
+# true? true.
+puts (10 < 20 && 30 < 20) || !(10 == 11)
